fa680ac8ec7425103965c697bffa1e0ba1c54c11
Amina Khatun <aminaemcbd@gmail.com>
2022-02-08 07:43:46 +0600

    sdaf

---
+++ android/app/src/main/kotlin/com/example/fluttercrud/flutter_crud_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttercrud.flutter_crud_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/sandbox/msg_sent.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_crud_test/helper/http_helper.dart';
+
+class MessagePage extends StatefulWidget {
+  const MessagePage({Key? key}) : super(key: key);
+
+  @override
+  _MessagePageState createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  final _formKey =GlobalKey<FormState>();
+  final _http = HttpHelper();
+  final _subject = TextEditingController();
+  final _content = TextEditingController();
+
+  save()async{
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Massage Box"),
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextFormField(
+                controller: _subject,
+                // controller: _subjectController(),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Subject',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+              child: TextFormField(
+                controller: _content
+
+
+
+                ,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Content',
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lightBlue, // background
+                    onPrimary: Colors.white, // foreground
+                  ),
+                  onPressed: () {
+                    if()
+                  },
+                  child: Text('send'),
+                ))
+          ],
+        ));
+  }
+}
+
+class _subjectController {
+}
